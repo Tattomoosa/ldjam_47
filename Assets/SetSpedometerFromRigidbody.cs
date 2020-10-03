e593b6862ced8e786369e62e9ed715c9e398ce7f
Matt O'Tousa <matt@mattotousa.com>
2020-10-02 20:36:08 -0700

    odometer + slightly improved car control

---
+++ Assets/SetSpedometerFromRigidbody.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSpedometerFromRigidbody : MonoBehaviour
+{
+    public Spedometer spedometer;
+    private Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO this should do something smarter
+        // Debug.Log(_rb.velocity.magnitude);
+        Vector3 velocity = _rb.velocity;
+        velocity.y = 0;
+        spedometer.currentValue = velocity.magnitude;
+    }
+}
---
+++ Assets/Spedometer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spedometer : MonoBehaviour
+{
+    public float currentValue = 30.0f;
+    public float minValue = 0.0f;
+    public float maxValue = 100.0f;
+    public float minRotation = 145.0f;
+    public float maxRotation = -145.0f;
+    
+    public RectTransform handleRootTransform;
+
+    void Start()
+    {
+        // _rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        float currentRotationLerp = Mathf.InverseLerp(minValue, maxValue, currentValue);
+        float rotationValue = Mathf.Lerp(minRotation, maxRotation, currentRotationLerp);
+        handleRootTransform.rotation = Quaternion.Euler(0, 0, rotationValue);
+    }
+}
