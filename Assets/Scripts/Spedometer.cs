5db84a0e5c5b0b453f5b2e0170173de396f0f0fa
Matt O'Tousa <matt@mattotousa.com>
2020-10-04 00:19:17 -0700

    better spedometer, placeholder weapon ui

--- Assets/Scripts/Spedometer.cs
+++ Assets/Scripts/Spedometer.cs
@@ -1,17 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spedometer : MonoBehaviour
 {
     // public float currentValue = 30.0f;
     public float minValue = 0.0f;
     public float maxValue = 100.0f;
-    public float minRotation = 145.0f;
-    public float maxRotation = -145.0f;
+    public float minRotation = -145.0f;
+    public float maxRotation = 145.0f;
+    public int textPoints = 12;
+    public float innerTextRadius = 100.0f;
 
     public Rigidbody target;
     public RectTransform handleRootTransform;
+    public Text textBasis;
+
+    void Awake()
+    {
+        textBasis = GetComponentInChildren<Text>();
+        
+        // this isn't really right but it'll wrok
+        float degreesTotal = minRotation - maxRotation;
+        float degreesBetween = degreesTotal / textPoints;
+        Debug.Log("degrees total: " + degreesTotal + " degreesBetween: " + degreesBetween);
+        for (int i = 0; i < textPoints; ++i)
+        {
+            CreateTextAtDegrees(minRotation - (degreesBetween * i));
+        }
+        Destroy(textBasis);
+    }
+
+    void CreateTextAtDegrees(float degrees)
+    {
+        var rotationLerp = Mathf.InverseLerp(minRotation, maxRotation, degrees);
+        var value = Mathf.Lerp(minValue, maxValue, rotationLerp);
+        Debug.Log("degrees: " + degrees + " lerp: " + rotationLerp + " value: " + value);
+        Vector3 noRotation = Vector3.up * innerTextRadius;
+        Vector3 rotation = Quaternion.AngleAxis(degrees, Vector3.forward) * noRotation;
+        var text = Object.Instantiate(textBasis);
+        text.transform.SetParent(this.transform);
+        text.transform.localPosition = rotation;
+        // text.text = Mathf.FloorToInt(value).ToString();
+        text.text = (Mathf.Round(value / 5) * 5).ToString();
+    }
 
     void Update()
     {
