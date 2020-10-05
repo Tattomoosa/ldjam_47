04776f229454ab572ec265bff2b717f38c827b54
Matt O'Tousa <matt@mattotousa.com>
2020-10-05 00:23:35 -0700

    updated title screen with hi score text

---
+++ Assets/LapHighScoreText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapHighScoreText : MonoBehaviour
+{
+    private Text _text;
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        if (PlayerPrefs.HasKey("hiScore"))
+        {
+            int hiScore = PlayerPrefs.GetInt("hiScore");
+            int randomHigher = Random.Range(2, 5);
+            if (hiScore > 0)
+            {
+                _text.text = "You made it to lap " + hiScore + "." +
+                             " I bet something good happens at lap " +
+                             (hiScore + randomHigher) + "!";
+            }
+        }
+        else
+        {
+            
+        }
+    }
+}
