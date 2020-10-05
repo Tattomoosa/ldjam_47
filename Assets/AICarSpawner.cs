e4763d5c5f28eed21b66bd555187e571745c7c61
Matt O'Tousa <matt@mattotousa.com>
2020-10-04 20:45:33 -0700

    title scrren bg

---
+++ Assets/AICarSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AICarSpawner : MonoBehaviour
+{
+    public GameObject spawnObject;
+    public WaypointManager waypointManager;
+    public float oddsToSpawn = 0.001f;
+
+    void Start()
+    {
+        spawnObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float chance = Random.Range(0.0f, 1.0f);
+        if (chance < oddsToSpawn)
+        {
+            GameObject obj = Instantiate(spawnObject, transform);
+            CarInputAI c = obj.GetComponent<CarInputAI>();
+            c.waypointManager = waypointManager;
+            obj.SetActive(true);
+            obj.transform.position = transform.position;
+        }
+    }
+}
---
+++ Assets/CarDeathZone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDeathZone : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
