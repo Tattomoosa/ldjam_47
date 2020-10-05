87ae5c3f5de014881b46ccb019d124a27632de7b
Matt O'Tousa <matt@mattotousa.com>
2020-10-04 23:53:45 -0700

    lap text system

---
+++ Assets/LapTextSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTextSystem : MonoBehaviour
+{
+    public GameObject textBox;
+    public Text text;
+    
+    public List<LapText> lapText = new List<LapText>();
+
+    private Dictionary<int, string> _lapTextDict = new Dictionary<int, string>();
+
+    void Awake()
+    {
+        textBox.SetActive(false);
+        foreach (LapText lt in lapText)
+            _lapTextDict[lt.lap] = lt.text;
+    }
+
+    public void OnLap(int lap)
+    {
+        if (!_lapTextDict.ContainsKey(lap))
+            return;
+        if (!textBox.activeSelf)
+            textBox.SetActive(true);
+        text.text = _lapTextDict[lap];
+    }
+
+    [System.Serializable]
+    public struct LapText
+    {
+        public int lap;
+        [TextArea]
+        public string text;
+    }
+}
